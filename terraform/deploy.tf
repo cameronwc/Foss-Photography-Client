e16c23c25d6ca6c245a8254bc0ae046a33e9fbbe
Cameron Cooper <cameron.w.cooper@gmail.com>
2021-07-21 09:03:46 -0600

    fix spacing

--- terraform/deploy.tf
+++
@@ -1,6 +0,0 @@
-# resource "null_resource" "deploy" {
-#   provisioner "local-exec" {
-#     command = "cd aws s3 sync build/* s3://${var.bucket_name}"
-#   }
-#   depends_on = [aws_s3_bucket.react_bucket]
-# }
